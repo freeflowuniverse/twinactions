afc8103e2cbe6cc43f770a550752fb686cfba151
timurgordon <timurgordon@gmail.com>
2022-08-04 13:46:06 +0200

    add go files to be implemented

---
+++ go/crypto/algorand.go
@@ -0,0 +1,14 @@
+
+func create_account() 
+
+func create_asset(asset_type, name, account)
+	if asset_type == nft
+	// TODO: Logic for creating nft
+
+func delete_asset()
+
+func mint_asset(asset_id)
+
+func burn_asset(asset_id)
+
+func transfer_asset()
