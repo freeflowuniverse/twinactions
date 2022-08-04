4a543dbd3656ef0a08bbfff3322b50aa3640744c
despiegk <>
2022-08-04 07:50:47 +0300

    github actions

--- doc.sh
+++ doc.sh
@@ -7,6 +7,7 @@ v fmt . -w
 v doc  -m  -f html  -no-timestamp -comments -o html2 twinactions/
 rm -rf html/
 mv html2/_docs/ html/
+rm -rf html2/
 if [ -x "$(command -v open)" ]; then
     open html/twinactions.html 2>&1 > /dev/null 
 fi
