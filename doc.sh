41891186bf93ea99cfa812de7a50d6cd704876ea
despiegk <>
2022-08-04 09:01:02 +0300

    specs

--- doc.sh
+++ doc.sh
@@ -5,13 +5,13 @@ rm -rf docs/src/v/_docs
 rm -rf html2/
 v fmt . -w
 v doc  -m  -f html  -no-timestamp -comments -o html2 twinactions/
-rm -rf html/
-mv html2/_docs/ html/
+rm -rf docs/v/
+mv html2/_docs/ docs/v/
 rm -rf html2/
 pushd docs
 bash run.sh
 popd
 if [ -x "$(command -v open)" ]; then
     open docs/book/index.html 2>&1 > /dev/null 
-    open html/twinactions.html 2>&1 > /dev/null 
+    open docs/v/crypto.html 2>&1 > /dev/null 
 fi
