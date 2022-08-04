b7709b07b60e24fb931f9b0021a654ad3654743f
despiegk <>
2022-08-04 07:58:40 +0300

    github actions

--- doc.sh
+++ doc.sh
@@ -8,6 +8,10 @@ v doc  -m  -f html  -no-timestamp -comments -o html2 twinactions/
 rm -rf html/
 mv html2/_docs/ html/
 rm -rf html2/
+pushd docs
+bash run.sh
+popd
 if [ -x "$(command -v open)" ]; then
-    open html/twinactions.html 2>&1 > /dev/null 
+    # open html/twinactions.html 2>&1 > /dev/null 
+    open docs/book/index.html 2>&1 > /dev/null 
 fi
--- docs/run.sh
+++ docs/run.sh
@@ -1,2 +1,2 @@
-mdbook build --open
+mdbook build
  
