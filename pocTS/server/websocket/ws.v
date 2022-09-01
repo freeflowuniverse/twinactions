385bae16b551d383987af4d875943354f43e897d
omarabdul3ziz <omarabdul3ziz@gmail.com>
2022-09-01 18:04:14 +0200

    wip

--- pocTS/server/websocket/ws.v
+++ pocTS/server/websocket/ws.v
@@ -60,67 +60,67 @@ fn handle_events(raw_msg &tw.RawMessage, mut c ws.Client)? {
 	if msg.event == 'client_connected'{
 		println(msg.event)
 
-	} else if msg.event == "sum_balances" {
+	// } else if msg.event == "sum_balances" {
 		
-		// machine := tw.Machine{'test2411822',
-		// 	2,
-		// 	[], 
-		// 	[], 
-		// 	false, 
-		// 	true, 
-		// 	2, 
-		// 	1024, 
-		// 	1, 
-		// 	"https://hub.grid.tf/tf-official-apps/base:latest.flist",
-		// 	"/sbin/zinit init",
-		// 	tw.Env{ssh_key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCa5srzZwh3ulajtY2EZ1SiPY1JNelcaP8O/FZqrnJi6OxAPijl0KzoNrzgemqxhAS/eIglBYbgQuw/Po15MtdMgXmfrtNgrZjQQtLxGFz5KmUbzawPGI7iRkN40LEo0y0hcGLV1G+YiNO+3YU7K5I+gos+04OUJe4HYjcp92nAEviqxa40po2f67KgP5xrZxaOpELZA/hIf1wCzCyTsdvu3k+hw1QlSTIso6WTcUw7LLssvxAs7JZ31kgx+L740xQJWsiVv/go3td0GuETfRSbfjBtOD/wIEHG5UtazOrR+8ukotqQ/ERWuyx1abaEKwro3fLunmjhfgDbnJYy7As1 ahmed@ahmed-Inspiron-3576"}
-		// 	}
-		// machines := tw.Machines {
-		// 	"test2411822", tw.Network{
-		// 		"10.20.0.0/16",
-		// 		"test24",
-		// 		false
-		// 	},
-		// 	[machine],
-		// 	"",
-		// 	""
-		// }
-		// response := client.import_wallet(name: "test241182", address: "", secret: "SACLKHTVC3K36GCW6EPN7DAXNLLWPWLSFDAMM4EV22KRF62JK342QISI")?
+	// 	// machine := tw.Machine{'test2411822',
+	// 	// 	2,
+	// 	// 	[], 
+	// 	// 	[], 
+	// 	// 	false, 
+	// 	// 	true, 
+	// 	// 	2, 
+	// 	// 	1024, 
+	// 	// 	1, 
+	// 	// 	"https://hub.grid.tf/tf-official-apps/base:latest.flist",
+	// 	// 	"/sbin/zinit init",
+	// 	// 	tw.Env{ssh_key: "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCa5srzZwh3ulajtY2EZ1SiPY1JNelcaP8O/FZqrnJi6OxAPijl0KzoNrzgemqxhAS/eIglBYbgQuw/Po15MtdMgXmfrtNgrZjQQtLxGFz5KmUbzawPGI7iRkN40LEo0y0hcGLV1G+YiNO+3YU7K5I+gos+04OUJe4HYjcp92nAEviqxa40po2f67KgP5xrZxaOpELZA/hIf1wCzCyTsdvu3k+hw1QlSTIso6WTcUw7LLssvxAs7JZ31kgx+L740xQJWsiVv/go3td0GuETfRSbfjBtOD/wIEHG5UtazOrR+8ukotqQ/ERWuyx1abaEKwro3fLunmjhfgDbnJYy7As1 ahmed@ahmed-Inspiron-3576"}
+	// 	// 	}
+	// 	// machines := tw.Machines {
+	// 	// 	"test2411822", tw.Network{
+	// 	// 		"10.20.0.0/16",
+	// 	// 		"test24",
+	// 	// 		false
+	// 	// 	},
+	// 	// 	[machine],
+	// 	// 	"",
+	// 	// 	""
+	// 	// }
+	// 	// response := client.import_wallet(name: "test241182", address: "", secret: "SACLKHTVC3K36GCW6EPN7DAXNLLWPWLSFDAMM4EV22KRF62JK342QISI")?
 
-		// response := client.deploy_machines(machines)? 
-		// println("address $response.address")
-		// response2 := client.balance_by_name("test241182")?
-		// println(response2[0].amount)
-		// response3 := client.list_wallets()?
-		// println(response3)
-		go fn [mut client]() {
-			addrs := [
-			"5CoairYXspX6MHeAFaJ9Ki3Fsj2sFXVHf11ymPXcsWX5a7Mw",
-			"5D8ByeiGwKJ5YiZZfKAduomE5fezcR4xMVEcwfTxYm67UBSE"
-			]
-			mut total := 0.0
-			for addr in addrs {
-				balance := client.get_balance(addr) or {
-					println("couldn't get balance: $err")
-					return
-				}
+	// 	// response := client.deploy_machines(machines)? 
+	// 	// println("address $response.address")
+	// 	// response2 := client.balance_by_name("test241182")?
+	// 	// println(response2[0].amount)
+	// 	// response3 := client.list_wallets()?
+	// 	// println(response3)
+	// 	go fn [mut client]() {
+	// 		addrs := [
+	// 		"5CoairYXspX6MHeAFaJ9Ki3Fsj2sFXVHf11ymPXcsWX5a7Mw",
+	// 		"5D8ByeiGwKJ5YiZZfKAduomE5fezcR4xMVEcwfTxYm67UBSE"
+	// 		]
+	// 		mut total := 0.0
+	// 		for addr in addrs {
+	// 			balance := client.get_balance(addr) or {
+	// 				println("couldn't get balance: $err")
+	// 				return
+	// 			}
 
-				total += balance.free
-			}
-			// here we decided to send the result back
-			// but we can do anything with this result
-			resp_msg := tw.Message{
-				id: "anyidforresponse" // any id for resp
-				event: "balance_result"
-				data: json.encode(total)
-			}
+	// 			total += balance.free
+	// 		}
+	// 		// here we decided to send the result back
+	// 		// but we can do anything with this result
+	// 		resp_msg := tw.Message{
+	// 			id: "anyidforresponse" // any id for resp
+	// 			event: "balance_result"
+	// 			data: json.encode(total)
+	// 		}
 
-			payload := json.encode(resp_msg)
-			client.ws.write_string(payload) or {
-				println("cannot send payload: $err")
-				return
-			}
-		}()
+	// 		payload := json.encode(resp_msg)
+	// 		client.ws.write_string(payload) or {
+	// 			println("cannot send payload: $err")
+	// 			return
+	// 		}
+	// 	}()
 	} else if msg.event == "deploy_vm_form" {
 
 		res := Response{
@@ -135,7 +135,7 @@ fn handle_events(raw_msg &tw.RawMessage, mut c ws.Client)? {
 		data := json2.raw_decode(msg.data)?
 		vm := data.as_map()
 
-		machines := tw.Machines{
+		machines := tw.MachinesModel{
 			name: vm["vm_name"]?.str()
 			network: tw.Network{
 				ip_range: '10.200.0.0/16'
@@ -166,7 +166,8 @@ fn handle_events(raw_msg &tw.RawMessage, mut c ws.Client)? {
 			log: json.encode(machines)
 		})?
 
-    	response := client.deploy_machines(machines)?
+    	response := client.machines_deploy(machines)?
+		println('finished')
 		echo(mut client, Response{
 			event: "echo"
 			log: json.encode(response)
